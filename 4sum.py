e64632543e393117d725abdbbd3d54f8e7ecbaf0
William Lee <senvey@gmail.com>
2014-08-01 09:57:40 -0700

    optimization: 4sum and longest palindromic substring

--- 4sum.py
+++ 4sum.py
@@ -3,12 +3,12 @@
 """
 4sum.py
 
-Created by  on 2014-07-21.
+Created by  on 2014-07-21; AC on 2014-07-31.
 Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 """
 
 # https://oj.leetcode.com/problems/4sum/
-# tags: hard, numbers, sum, hashtable, search, dups, logic
+# tags: hard, numbers, sum, hashtable, search, dups, logic, optimization
 
 """
 Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
@@ -24,11 +24,31 @@ The solution set must not contain duplicate quadruplets.
     (-2,  0, 0, 2)
 """
 
+
+############ AC version ############
 # https://oj.leetcode.com/discuss/3950/tle-on-4sum-using-hashtable
+# 10 loops, best of 3: 27.2 ms per loop
+
+class Solution:
+    # @return a list of lists of length 4, [[val1,val2,val3,val4]]
+    def fourSum(self, nums, target):
+        nums = sorted(nums)
+        result = set()
+        cache  = collections.defaultdict(set)
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for half in cache[target - nums[i] - nums[j]]:
+                    result.add(tuple(list(half) + [nums[i], nums[j]]))
+            
+            for j in range(i):
+                cache[nums[i] + nums[j]].add((nums[j], nums[i]))
+        
+        return map(list, result)
 
 
 ############ without two sum but using `set` (faster) ############
-# TLE on test case: [91277418,66271374,38763793,4092006,11415077,60468277,1122637,72398035,-62267800,22082642,60359529,-16540633,92671879,-64462734,-55855043,-40899846,88007957,-57387813,-49552230,-96789394,18318594,-3246760,-44346548,-21370279,42493875,25185969,83216261,-70078020,-53687927,-76072023,-65863359,-61708176,-29175835,85675811,-80575807,-92211746,44755622,-23368379,23619674,-749263,-40707953,-68966953,72694581,-52328726,-78618474,40958224,-2921736,-55902268,-74278762,63342010,29076029,58781716,56045007,-67966567,-79405127,-45778231,-47167435,1586413,-58822903,-51277270,87348634,-86955956,-47418266,74884315,-36952674,-29067969,-98812826,-44893101,-22516153,-34522513,34091871,-79583480,47562301,6154068,87601405,-48859327,-2183204,17736781,31189878,-23814871,-35880166,39204002,93248899,-42067196,-49473145,-75235452,-61923200,64824322,-88505198,20903451,-80926102,56089387,-58094433,37743524,-71480010,-14975982,19473982,47085913,-90793462,-33520678,70775566,-76347995,-16091435,94700640,17183454,85735982,90399615,-86251609,-68167910,-95327478,90586275,-99524469,16999817,27815883,-88279865,53092631,75125438,44270568,-23129316,-846252,-59608044,90938699,80923976,3534451,6218186,41256179,-9165388,-11897463,92423776,-38991231,-6082654,92275443,74040861,77457712,-80549965,-42515693,69918944,-95198414,15677446,-52451179,-50111167,-23732840,39520751,-90474508,-27860023,65164540,26582346,-20183515,99018741,-2826130,-28461563,-24759460,-83828963,-1739800,71207113,26434787,52931083,-33111208,38314304,-29429107,-5567826,-5149750,9582750,85289753,75490866,-93202942,-85974081,7365682,-42953023,21825824,68329208,-87994788,3460985,18744871,-49724457,-12982362,-47800372,39958829,-95981751,-71017359,-18397211,27941418,-34699076,74174334,96928957,44328607,49293516,-39034828,5945763,-47046163,10986423,63478877,30677010,-21202664,-86235407,3164123,8956697,-9003909,-18929014,-73824245], -236727523
+# 10 loops, best of 3: 158 ms per loop. TLE on test case: [91277418,66271374,38763793,4092006,11415077,60468277,1122637,72398035,-62267800,22082642,60359529,-16540633,92671879,-64462734,-55855043,-40899846,88007957,-57387813,-49552230,-96789394,18318594,-3246760,-44346548,-21370279,42493875,25185969,83216261,-70078020,-53687927,-76072023,-65863359,-61708176,-29175835,85675811,-80575807,-92211746,44755622,-23368379,23619674,-749263,-40707953,-68966953,72694581,-52328726,-78618474,40958224,-2921736,-55902268,-74278762,63342010,29076029,58781716,56045007,-67966567,-79405127,-45778231,-47167435,1586413,-58822903,-51277270,87348634,-86955956,-47418266,74884315,-36952674,-29067969,-98812826,-44893101,-22516153,-34522513,34091871,-79583480,47562301,6154068,87601405,-48859327,-2183204,17736781,31189878,-23814871,-35880166,39204002,93248899,-42067196,-49473145,-75235452,-61923200,64824322,-88505198,20903451,-80926102,56089387,-58094433,37743524,-71480010,-14975982,19473982,47085913,-90793462,-33520678,70775566,-76347995,-16091435,94700640,17183454,85735982,90399615,-86251609,-68167910,-95327478,90586275,-99524469,16999817,27815883,-88279865,53092631,75125438,44270568,-23129316,-846252,-59608044,90938699,80923976,3534451,6218186,41256179,-9165388,-11897463,92423776,-38991231,-6082654,92275443,74040861,77457712,-80549965,-42515693,69918944,-95198414,15677446,-52451179,-50111167,-23732840,39520751,-90474508,-27860023,65164540,26582346,-20183515,99018741,-2826130,-28461563,-24759460,-83828963,-1739800,71207113,26434787,52931083,-33111208,38314304,-29429107,-5567826,-5149750,9582750,85289753,75490866,-93202942,-85974081,7365682,-42953023,21825824,68329208,-87994788,3460985,18744871,-49724457,-12982362,-47800372,39958829,-95981751,-71017359,-18397211,27941418,-34699076,74174334,96928957,44328607,49293516,-39034828,5945763,-47046163,10986423,63478877,30677010,-21202664,-86235407,3164123,8956697,-9003909,-18929014,-73824245], -236727523
 
 class Solution:
     # @return a list of lists of length 4, [[val1,val2,val3,val4]]
@@ -36,19 +56,19 @@ class Solution:
         nums = sorted(nums)
         
         result = set()
-        for index in xrange(1, len(nums) - 2):
+        for b in xrange(1, len(nums) - 2):
             # stores sub_sums for the new num index just moved on
             sub_sums = {}
             
-            for i in xrange(index):
-                sub_sums[nums[i] + nums[index]] = [nums[i], nums[index]]
+            for a in xrange(b):
+                sub_sums[nums[a] + nums[b]] = [nums[a], nums[b]]
             
-            for cursor in xrange(index + 1, len(nums) - 1):
-                for i in xrange(cursor + 1, len(nums)):
-                    remainder = target - nums[cursor] - nums[i]
+            for c in xrange(b + 1, len(nums) - 1):
+                for d in xrange(c + 1, len(nums)):
+                    remainder = target - nums[c] - nums[d]
                     if remainder in sub_sums:
                         result.add(tuple(
-                           sub_sums[remainder]  + [nums[cursor], nums[i]]))
+                           sub_sums[remainder]  + [nums[c], nums[d]]))
         
         return list(map(list, result))
 
@@ -73,8 +93,44 @@ class Solution:
         
         return list(map(list, result))
 
+# optimized two sum version with about same running time
 
-############ faster two sum O(n^2) ############
+class Solution:
+    # @return a list of lists of length 4, [[val1,val2,val3,val4]]
+    def fourSum(self, nums, target):
+        nums = sorted(nums)
+        result = []
+        
+        for a in xrange(len(nums) - 3):
+            if a > 0 and nums[a] == nums[a - 1]: continue
+        
+            for b in xrange(a + 1, len(nums) - 2):
+                if b > a + 1 and nums[b] == nums[b - 1]: continue
+                
+                # two sum for the other half
+                half = nums[a] + nums[b]
+                c, d = b + 1, len(nums) - 1
+                while c < d:
+                    total = half + nums[c] + nums[d]
+                    if total == target:
+                        result.append([nums[a], nums[b], nums[c], nums[d]])
+                        c += 1
+                        d -= 1
+                        # inner loop is almost twice faster than if..continue
+                        # for checking each num in nums[b + 1...len(nums) - 1]
+                        while c < d and nums[c] == nums[c - 1]:
+                            c += 1
+                        while c < d and nums[d] == nums[d + 1]:
+                            d -= 1
+                    elif total < target:
+                        c += 1
+                    else:
+                        d -= 1
+        
+        return result
+
+
+############ faster two sum O(n^3) ############
 # using `set` instead of control dups manually
 
 class Solution:
@@ -113,8 +169,7 @@ class Solution:
         return list(map(list, result))
 
 
-
-############ two sum O(n^2) ############
+############ two sum O(n^3) ############
 
 class Solution:
     # @return a list of lists of length 4, [[val1,val2,val3,val4]]
@@ -124,7 +179,7 @@ class Solution:
         def two_sum(start, target):
             left, right = start, len(nums) - 1
             while left < right:
-                if left > start and nums[left] == nums[start-1]:
+                if left > start and nums[left] == nums[left-1]:
                     left += 1
                     continue
                 if right < len(nums) - 1 and nums[right] == nums[right+1]:
@@ -170,6 +225,8 @@ class Solution:
         
         def generator():
             for index in xrange(1, len(nums) - 2):
+                # incorrect: it skips the possibility of combination with
+                #   nums[index - 1], nums[index] and two other bigger numbers
                 if nums[index] == nums[index - 1]:
                     continue
                 
@@ -186,7 +243,8 @@ class Solution:
                     if cursor > index + 1 and nums[cursor] == nums[cursor - 1]:
                         continue
                     for i in xrange(cursor + 1, len(nums)):
-                        if nums[i] == nums[i - 1]:
+                        # note: similar situation as above
+                        if i > cursor + 1 and nums[i] == nums[i - 1]:
                             continue
                         remainder = target - nums[cursor] - nums[i]
                         if remainder in sub_sums:
@@ -194,3 +252,27 @@ class Solution:
         
         return list(generator())
 
+############ plain three loops O(n^3) ############
+# 1 loops, best of 3: 506 ms per loop
+
+class Solution:
+    # @return a list of lists of length 4, [[val1,val2,val3,val4]]
+    def fourSum(self, nums, target):
+        nums = sorted(nums)
+        result = []
+
+        for a in xrange(len(nums) - 3):
+            if a > 0 and nums[a] == nums[a - 1]: continue
+
+            for b in xrange(a + 1, len(nums) - 2):
+                if b > a + 1 and nums[b] == nums[b - 1]: continue
+
+                rest = set(nums[b + 1:])
+                for c in xrange(b + 1, len(nums) - 1):
+                    if c > b + 1 and nums[c] == nums[c - 1]: continue
+                    rest.remove(nums[c])
+                    d = target - nums[a] - nums[b] - nums[c]
+                    if d in rest:
+                        result.append([nums[a], nums[b], nums[c], d])
+
+        return result
--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -3,11 +3,12 @@
 """
 longest_palindrome_substring.py
 
-Created by  on 2014-07-16.
+Created by  on 2014-07-16; updated on 2014-07-31.
 Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 """
 
 # https://oj.leetcode.com/problems/longest-palindromic-substring/
+# tags: hard, string, palindrome, optimization
 
 """
 Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
@@ -17,29 +18,157 @@ Given a string S, find the longest palindromic substring in S. You may assume th
 # TODO
 
 ############# O(n^2) #############
-
+# http://blog.shengwei.li/leetcode-longest-palindromic-substring/
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
         if s is None or len(s) == 0:
             return ''
+
+        half = len(s) / 2
+
+        def max_palindrome(left, right, min_length):
+            """Gets the longest palindrom in s[left:right+1] centered at
+            index (left + right) / 2"""
+            if right - left < min_length:
+                return ''
+            start = left
+            while left < right:
+                if s[left] != s[right]:
+                    if right - left < min_length:
+                        return ''
+                    start = left + 1
+                left += 1
+                right -= 1
+
+            length = (left - start) * 2 + (right - left + 1)
+            return s[start:start+length]
+
+        def is_palindrome(left, right):
+            """Checks if s[left:right+1] is a palindrome."""
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        def get_max(left, cur_s, right, max_palin):
+            """Gets maximum extensible boundary as a palindrome
+            centered with cur_s."""
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                cur_s = s[left] + cur_s + s[right]
+                left -= 1
+                right += 1
+
+            return cur_s if len(cur_s) > len(max_palin) else max_palin
+        
+        def process_short(max_palin):
+            for distance in xrange(half + 1):
+                for sign in (-1, 1):
+                    i = half + sign * distance
+                    # even palindrome
+                    max_palin = get_max(i - 1, '', i, max_palin)
+                    # odd palindrome
+                    if i < len(s):
+                        max_palin = get_max(i - 1, s[i], i + 1, max_palin)
+                
+                max_half = min(i, len(s) - i)
+                if len(max_palin) == max_half * 2:
+                    return max_palin
+            
+            return max_palin
+        
+        if len(s) <= 500:
+            return process_short(s[0])
+
+        if is_palindrome(0, len(s) - 1):
+            return s
+        max_palin = ''
+        for i in range(1, half):
+            if is_palindrome(i, len(s) - 1) and len(s[i:len(s)]) > len(max_palin):
+                max_palin = s[i:len(s)]
+        for j in range(half, len(s) - 1):
+            if is_palindrome(0, j) and len(s[:j + 1]) > len(max_palin):
+                max_palin = s[:j + 1]
+
+        if not max_palin:
+            cur_max = self.longestPalindrome(s[:half])
+            if len(cur_max) > len(max_palin):
+                max_palin = cur_max
+            cur_max = self.longestPalindrome(s[half:])
+            if len(cur_max) > len(max_palin):
+                max_palin = cur_max
+
+        if len(max_palin) >= half:
+            return max_palin
+        
+        return process_short(max_palin)
+
+############# Optimized #############
+class Solution:
+    # @return a string
+    def longestPalindrome(self, s):
+        if s is None or len(s) == 0:
+            return ''
+            
+        def max_palindrome(left, right, min_length):
+            if right - left < min_length:
+                return ''
+            start = left
+            while left < right:
+                if s[left] != s[right]:
+                    if right - left < min_length:
+                        return ''
+                    start = left + 1
+                left += 1
+                right -= 1
+            
+            length = (left - start) * 2 + (right - left + 1)
+            return s[start:start+length]
+        
+        max_palin = s[0]
+        for distance in xrange(len(s) / 2):
+            # distance needs to be (len(s) / 2) to reach index 0 but we don't
+            # need to process index 0 as it's the center of palindrome itself
+            for sign in (-1, 1):
+                i = len(s) / 2 + sign * distance
+                
+                # odd palindrome
+                max_half = min(i, len(s) - i - 1)
+                cur_max = max_palindrome(i - max_half, i + max_half, len(max_palin))
+                if len(cur_max) > len(max_palin):
+                    max_palin = cur_max
+                
+                # even palindrome
+                max_half = min(i, len(s) - i)
+                cur_max = max_palindrome(i - max_half, i + max_half - 1, len(max_palin))
+                if len(cur_max) > len(max_palin):
+                    max_palin = cur_max
+        
+        return max_palin
+
+############# naive solution #############
+class Solution:
+    # @return a string
+    def longestPalindrome(self, s):
+        if s is None or len(s) == 0:
+            return ''
+
+        def get_max(left, cur_s, right, max_palin):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                cur_s = s[left] + cur_s + s[right]
+                left -= 1
+                right += 1
+
+            return cur_s if len(cur_s) > len(max_palin) else max_palin
         
         max_palin = s[0]
         for i in xrange(1, len(s)):
-            
-            def get_max(left, cur_s, right, max_palin):
-                while left >= 0 and right < len(s) and s[left] == s[right]:
-                    cur_s = s[left] + cur_s + s[right]
-                    left -= 1
-                    right += 1
-                
-                return cur_s if len(cur_s) > len(max_palin) else max_palin
-            
             # even palindrome
             max_palin = get_max(i - 1, '', i, max_palin)
-            
             # odd palindrome
             max_palin = get_max(i - 1, s[i], i + 1, max_palin)
         
         return max_palin
-            
+
