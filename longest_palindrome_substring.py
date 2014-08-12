b86b590eb63d60134ff54a70e47886ef84eaf158
William Lee <senvey@gmail.com>
2014-08-12 10:13:43 -0700

    added a better version found online

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -16,6 +16,29 @@ Given a string S, find the longest palindromic substring in S. You may assume th
 ############# O(n) #############
 # TODO
 
+
+# http://www.mitbbs.com/article/JobHunting/32755595_0.html
+class Solution:
+    def longestPalindrome(self, s):
+        longest, mid = "", (len(s) - 1) / 2
+        i, j = mid, mid
+        while i >= 0 and j < len(s):
+            args = [(s, i, i), (s, i, i + 1), (s, j, j), (s, j, j + 1)]
+            for arg in args:
+                tmp = self.longestPalindromeByAxis(*arg)
+                if len(tmp) > len(longest):
+                    longest = tmp
+            if len(longest) >= i * 2:
+                return longest
+            i, j = i - 1, j + 1
+        return longest
+        
+    def longestPalindromeByAxis(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left, right = left - 1, right + 1
+        return s[left + 1 : right]
+
+
 ############# O(n^2) #############
 # http://blog.shengwei.li/leetcode-longest-palindromic-substring/
 class Solution:
